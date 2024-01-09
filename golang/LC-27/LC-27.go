9a737cacf235a06bfa0f15ab94b4d4d7616b95df
vrajd191@gmail.com <vraj@vrajs-mbp.lan>
2024-01-08 22:26:56 -0500

    Leet Code Problem 26

--- golang/LC-27/LC-27.go
+++ golang/LC-27/LC-27.go
@@ -1,20 +1,3 @@
-package main
-
-import "fmt"
-
-func main() {
-
-	/*
-		This contains the solution for LeetCode problem 27 - Remove element
-	*/
-
-	val1 := removeElement([]int{3, 2, 2, 3}, 3)
-	val2 := removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2)
-
-	fmt.Println(val1, val2)
-
-}
-
 func removeElement(nums []int, val int) int {
 	counter := 0
 	for _, value := range nums {
