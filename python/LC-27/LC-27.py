8d747709c7ad140445ff59fe57e6577bd52f0c38
vrajd191@gmail.com <vraj@vrajs-mbp.lan>
2024-03-03 17:54:08 -0500

    Refreshed the list and starting to do all Neetcode problems

--- python/LC-27/LC-27.py
+++
@@ -1,9 +0,0 @@
-import ast as List
-
-# This contains the solution for LeetCode problem 27 - Remove element
-def removeElement(self, nums: List[int], val: int) -> int:
-        while val in nums:
-            nums.remove(val)
-
-
-
--- python/LC-80/LC-80.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        k = 0
-        for i in nums:
-            if k < 2 or i != nums[k-2]:
-                nums[k] = i
-                k += 1
-        return k
